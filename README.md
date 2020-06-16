011d0eee26f51f7eee7fb2b209549710cc430ce9
mamazu <maxo.pech@gmail.com>
2020-06-16 11:59:32 +0200

    Inital commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Monolog Markdown Formatter
+A formatter that prints the stacktrace in markdown
+
+## Usage
+`composer require brille24/markdown-formatter`
+
+Define the class `Brille24\MarkdownFormatter\MarkdownFormatter` as a service and use it.
---
+++ src/MarkdownFormatter.php
@@ -0,0 +1,138 @@
+<?php
+declare(strict_types=1);
+
+namespace Brille24\MarkdownFormatter;
+
+use Exception;
+use Monolog\Formatter\FormatterInterface;
+
+final class MarkdownFormatter implements FormatterInterface
+{
+    /** @var string */
+    private $projectPath;
+
+    /** @var array<string, string> */
+    private $logLevelSymbols = [
+        'INFO' => ':information_source:',
+        'DEBUG' => ':information_source:',
+        'WARN' => '### :warning:',
+        'ERROR' => '## :exclamation:',
+    ];
+
+    public function __construct(string $projectPath, ?array $logLevelSymbols = null)
+    {
+        $this->projectPath = $projectPath;
+
+        if ($logLevelSymbols !== null) {
+            $this->logLevelSymbols = $logLevelSymbols;
+        }
+    }
+
+    /** {@inheritDoc} */
+    public function format(array $record)
+    {
+        $errorSymbol = $this->logLevelSymbols[$record['level_name']] ?? '';
+        $headline = $errorSymbol.' '.$this->formatMessage($record).PHP_EOL;
+
+        $stacktrace = $this->formatStackTrace($record);
+        $context = $this->formatContext($record['context']);
+
+        return sprintf(
+            <<<MARKDOWN
+%s
+
+%s
+
+**Context:**
+```json
+%s
+```
+MARKDOWN, $headline, $stacktrace, $context
+        );
+    }
+
+    /**
+     * Formats a set of log records.
+     *
+     * @param array $records A set of records to format
+     *
+     * @return mixed The formatted set of records
+     */
+    public function formatBatch(array $records)
+    {
+        $message = '';
+        foreach ($records as $record) {
+            $message .= $this->format($record).PHP_EOL.PHP_EOL;
+        }
+
+        return $message;
+    }
+
+    /** @param array<string, mixed> $context */
+    private function formatContext(array $context): string
+    {
+        unset($context['exception']);
+
+        if (count($context) > 0) {
+            return json_encode($context, JSON_PRETTY_PRINT);
+        }
+
+        return '';
+    }
+
+    private function replacePath(string $absolutePath): string
+    {
+        $projectRoot = $this->projectPath;
+        if (strpos($absolutePath, $projectRoot) === 0) {
+            return substr($absolutePath, strlen($projectRoot) + 1);
+        }
+
+        return $absolutePath;
+    }
+
+    private function formatMessage(array &$record): string
+    {
+        $message = $record['message'];
+
+        foreach ($record['context'] as $key => $context) {
+            $searchTerm = "{{$key}}";
+            if (strpos($message, $searchTerm) !== false) {
+                $message = str_replace($searchTerm, (string)$context, $message);
+                unset($record['context'][$key]);
+            }
+        }
+
+        return $message;
+    }
+
+    private function formatStackTrace(array &$record): string
+    {
+        $message = '';
+        if (array_key_exists('exception', $record['context'])) {
+            /** @var Exception $exception */
+            $exception = $record['context']['exception'];
+            $message .= '| Function | Location |'.PHP_EOL;
+            $message .= '|----------|----------|'.PHP_EOL;
+            foreach ($exception->getTrace() as $trace) {
+                $message .= '|';
+                $class = $trace['class'] ?? '\\';
+                $type = $trace['type'] ?? '';
+                if (array_key_exists('function', $trace)) {
+                    $function = $trace['function'].'()';
+                } else {
+                    $function = '<no-function>';
+                }
+
+                $message .= $class.$type.$function.' | ';
+                if (array_key_exists('file', $trace)) {
+                    $line = $trace['line'] ?? '';
+                    $message .= $this->replacePath($trace['file']).':';
+                    $message .= $line.' |'.PHP_EOL;
+                }
+            }
+            $message .= PHP_EOL;
+        }
+
+        return $message;
+    }
+}
